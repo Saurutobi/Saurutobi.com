8c3ac0f105a3a03d34d365d0b2e8014aac015129
Saurutobi <saurutobi@gmail.com>
2015-10-15 22:46:02 -0700

    got media thing to work well enough

--- js/media.js
+++ js/media.js
@@ -1,7 +1,19 @@
-1: "https://docs.google.com/spreadsheets/d/1hXb3W_M__uBLGUePdgzHWgITGG1KnIQuT5GYIvFISsw/pubhtml?widget=true&amp;headers=false"
-2: "https://docs.google.com/spreadsheets/d/1dcM6jYrMiDkaw3N2Iz9w37-VA5DbdPW-WWLVyekUWVk/pubhtml?widget=true&amp;headers=false"
-3: "https://docs.google.com/spreadsheets/d/1aFgTuX1f8P1ch0s1DVFeBXVMc7HbRqFa-820QmkBvZM/pubhtml?widget=true&amp;headers=false"
-4: "https://docs.google.com/spreadsheets/d/1COGK6XtuHeaUtzCj87_fsGYoRQ_7fKEnilII0yuYaGM/pubhtml?widget=true&amp;headers=false"
-
-
-
+function ChangeMediaDrive(drive)
+{
+    if(drive == 1)
+    {
+        $("#ActiveMediaDrive").attr("src", "https://docs.google.com/spreadsheets/d/1hXb3W_M__uBLGUePdgzHWgITGG1KnIQuT5GYIvFISsw/pubhtml?widget=true&amp;headers=false");
+    }
+    else if(drive == 2)
+    {
+        $("#ActiveMediaDrive").attr("src", "https://docs.google.com/spreadsheets/d/1dcM6jYrMiDkaw3N2Iz9w37-VA5DbdPW-WWLVyekUWVk/pubhtml?widget=true&amp;headers=false");
+    }
+    else if(drive == 3)
+    {
+        $("#ActiveMediaDrive").attr("src", "https://docs.google.com/spreadsheets/d/1aFgTuX1f8P1ch0s1DVFeBXVMc7HbRqFa-820QmkBvZM/pubhtml?widget=true&amp;headers=false");
+    }
+    else if(drive == 4)
+    {
+        $("#ActiveMediaDrive").attr("src", "https://docs.google.com/spreadsheets/d/1COGK6XtuHeaUtzCj87_fsGYoRQ_7fKEnilII0yuYaGM/pubhtml?widget=true&amp;headers=false");
+    }
+}
